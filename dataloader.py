008066a695b914b809d216aba485b58b7bdb78ef
PoH0716 <phh003@ucsd.edu>
2021-03-08 17:26:58 -0800

    Update dataloader

--- dataloader.py
+++ dataloader.py
@@ -2,16 +2,28 @@
 import logging
 
 import torch
-import torchvision
-from torchvision.transforms import Compose, ToTensor, Resize
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
 
 def get_dataloader(split, batch_size, shape, num_workers):
     logging.info(f"Getting {split} dataloader.")
-    transform = Compose([Resize(shape), ToTensor()])  # TODO: add other transforms/data augmentation
-    celeba_data = torchvision.datasets.CelebA('.', split=split, transform=transform, download=True)
+    
+    # download the data set and apply transformation without cropping
+    transform = transforms.Compose([transforms.Resize(shape), transforms.ToTensor()])
+    celeba_data = dset.CelebA('.', split=split, transform=transform, download=True)
+    
+    # load the dataset without downloading, images are center cropped
+    dataroot = "celeba"
+    transform = transforms.Compose([transforms.Resize(shape),
+                                    transforms.CenterCrop(shape),
+                                    transforms.ToTensor(),
+                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    dataset = dset.ImageFolder(root=dataroot, transform=transform)
+    
+    # change `celeba_data` to `dataset`
     data_loader = torch.utils.data.DataLoader(celeba_data,
-                                            batch_size=batch_size,
-                                            shuffle=True,
-                                            num_workers=num_workers,
-                                            pin_memory=torch.cuda.is_available())
+                                              batch_size=batch_size,
+                                              shuffle=True,
+                                              num_workers=num_workers,
+                                              pin_memory=torch.cuda.is_available())
     return data_loader
